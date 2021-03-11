48f00fa42baaab9f88a2f4e9d708432fb4712a88
bens1320 <yu1piao09@163.com>
2021-03-12 00:06:57 +0800

    新加上 ES6 的作用域，使得 js 模块有自己独立的变量

--- 7-ES6Module/1.js
+++ 7-ES6Module/1.js
@@ -1,3 +1,7 @@
-window.module1 = function (){
-  alert(1)
+{
+  let name = 'Jonathan Ben'
+  let age = 24
+  window.module1 = function () {
+    alert(`你好，我是${name}，今年 ${age} 岁了。`)
+  }
 }
--- 7-ES6Module/2.js
+++ 7-ES6Module/2.js
@@ -1,3 +1,8 @@
-window.module2 = function (){
-  alert(2)
+{
+  let name = 'caoyi'
+  let age = 25
+  window.module2 = function () {
+    alert(`你好，我是${name}，今年 ${age} 岁了。`)
+
+  }
 }
